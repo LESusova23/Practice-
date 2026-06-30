076c36b9e58739dcc656112f28b70667e955bcf5
LESusova23 <LESusova23@codingburgas.bg>
2026-07-01 00:18:49 +0300

    Finish CRUD functionality for ships and cleanup code

--- app.py
+++ app.py
@@ -7,22 +7,31 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-
-        ship_name = request.form['name']
-        reg_num = request.form['reg_number']
-        captain_name = request.form['captain']
+        # Используем .get для безопасности
+        ship_name = request.form.get('name', '')
+        reg_num = request.form.get('reg_number', '')
+        captain_name = request.form.get('captain', '')
 
         new_ship = Ship(name=ship_name, reg_number=reg_num, captain=captain_name)
-
         db.session.add(new_ship)
         db.session.commit()
         return redirect(url_for('index'))
 
     ships = Ship.query.all()
     return render_template('index.html', ships=ships)
+
+
+@app.route('/delete/<int:ship_id>', methods=['GET', 'POST'])
+def delete_ship(ship_id: int):
+    # Теперь переменная называется ship_id, конфликтов нет
+    ship = Ship.query.get_or_404(ship_id)
+    db.session.delete(ship)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
